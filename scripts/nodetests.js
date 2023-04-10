35413914bd3448e5a81fec28531a5a0d5d67923f
neridash <122698655+ashadwe@users.noreply.github.com>
2023-04-10 18:38:08 +0200

    testing jquery and nodejs

--- scripts/nodetests.js
+++ scripts/nodetests.js
@@ -1,6 +1,5 @@
 var http = require('http');
-
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+  res.writeHead(200);
+  res.end();
+}).listen(8080); 
