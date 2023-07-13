6fcd97c486adc37155bd83f9e15d159613441fe9
Ash <ashwsng@proton.me>
2023-07-13 18:37:00 +0200

    alot

---
+++ scripts/weather.js
@@ -0,0 +1,11 @@
+const fetchButton = document.getElementById("fetchButton");
+var countryField = document.getElementById("country");
+var cityField = document.getElementById("city");
+
+function fetchWeather() {
+
+};
+
+function displayWeather() {
+
+};
